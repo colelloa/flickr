fe323acfeee79e04ad7dfdb60db925e3bd2ed08e
Andrew <agcolello@gmail.com>
2016-03-04 02:10:31 -0500

    i am mostly done

--- flickr/sandbox.py
+++ flickr/sandbox.py
@@ -4,9 +4,19 @@ from math import sqrt
 from skimage.color import rgb2gray
 from skimage.io import imread
 
+import system_constants as s
+
+
 URL = 'https://farm3.staticflickr.com/2598/3753084645_9136798d32.jpg'
 URL2 = 'https://farm2.staticflickr.com/1102/5117093816_a9c4c383c9.jpg'
 URL3 = 'https://farm3.staticflickr.com/2643/3753861818_662a81c0c6.jpg'
+
+URL4='https://farm3.staticflickr.com/2294/1975875218_0610e6e722.jpg'
+
+PATH_CLASSIFIED='/home/andrew/classes/thesis/python/flickr/pictures/train.arff'
+PATH_UNCLASSIFIED = '/home/andrew/classes/thesis/python/flickr/pictures/golfballs.arff'
+PATH_WRITE='/home/andrew/classes/thesis/python/flickr/pictures/test.arff'
+
 def create_blobs(url, opt='all'):
     image = imread(url)
     return_dict = dict()
@@ -75,4 +85,72 @@ def create_blobs(url, opt='all'):
     plt.show()
     return return_dict
 
-create_blobs(URL3, 'log')
+
+#convert from arff to a list of dicts
+def arff_to_dict(path):
+    data = False
+    all_list = list()
+    with open(path) as f:
+        for line in f:
+            ret = dict()
+            if(data):
+                l = line.split(',')
+                ret['url'] = l[0]
+                ret['algorithm'] = l[1]
+                ret['xcenter'] = l[2]
+                ret['ycenter'] = l[3]
+                ret['meanpx']=l[4]
+                ret['medianpx']=l[5]
+                ret['modepx']=l[6]
+                ret['radius']=l[7]
+                ret['radiusheightpct']=l[8]
+                ret['radiuswidthpct']=l[9]
+                ret['class']=l[10]
+
+
+                all_list.append(ret)
+            if '@DATA' in line:
+                data = True
+            
+    return all_list
+
+#write list of dicts to path
+def write_array_dict(target, path):
+    with open(path,'a') as f:
+        f.write(s.ARFF_HEADER)
+        for item in target:
+            f.write('{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10}\n'.format(
+                        item['url'], item['algorithm'], item['xcenter'], item['ycenter'], item['meanpx'], item['medianpx'],
+                        item['modepx'], item['radius'], item['radiusheightpct'], item['radiuswidthpct'], item['class']))  
+
+def find_classified(entry, cla):
+    match = None
+    q_url = entry['url']
+    for e in cla:
+        if e['url'] == q_url:
+            match = e
+    return match
+
+def classify_set(path_unclassified, path_classified, threshold):
+    unc = arff_to_dict(path_unclassified)
+    cla = arff_to_dict(path_classified)
+    count = 0
+    for entry in unc:
+        match = find_classified(entry, cla)
+        if(match):
+            count +=1
+
+            if not('?' in entry['xcenter'] or '?' in entry['ycenter']):
+                if((abs(int(entry['ycenter']) - int(match['ycenter'])) < threshold) and
+                    (abs(int(entry['xcenter']) - int(match['xcenter'])) < threshold)
+                    ):
+                    entry['class'] = match['class']
+
+            if '?' in entry['class']:
+                entry['class'] = 'NEGATIVE'
+
+    write_array_dict(unc, PATH_WRITE)
+
+classify_set(PATH_UNCLASSIFIED, PATH_CLASSIFIED, 50)
+# create_blobs(URL4)
+
